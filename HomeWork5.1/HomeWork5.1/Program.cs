35a82a9210c23d3c7be6f91309b78fb9b1fabd6a
Julia <109028438+Kyralesova@users.noreply.github.com>
2022-07-27 22:44:07 +0300

    Add files via upload

---
+++ HomeWork5.1/HomeWork5.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeWork5._1
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод принимающий строку, возвращающий массив строк и выводящий в консоль
+        /// </summary>
+        /// <param name="text">Строковая переменная, которую нужно разделить в массив</param>
+        static void SplitWords(string text)
+        {
+            string[] arrayWord = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var e in arrayWord)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string text = Console.ReadLine();
+
+            SplitWords(text);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork5.11/HomeWork5.11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWork5._11
+{
+    class Program
+    {   /// <summary>
+        /// Метод принимающий строку и возвращающий массив строк
+        /// </summary>
+        /// <param name="text">Строковая переменная, которую нужно разделить в массив</param>
+        static string[] SplitWords(string text)
+        {
+            string[] arrayWord = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return arrayWord;
+        }
+        /// <summary>
+        /// Метод вывода массива в консоль
+        /// </summary>
+        /// <param name="arrayWord">Метод вывода массива в консоль</param>
+        static void Print(string[] arrayWord)
+        {
+            foreach (string e in arrayWord)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string text = Console.ReadLine();
+
+            string[] arrayWord = SplitWords(text);
+            Print(arrayWord);
+
+            Console.ReadKey();
+        }
+    }
+}
