82cbbba0494f188005091480314dfdb2a46efa73
Julia <109028438+Kyralesova@users.noreply.github.com>
2022-07-30 19:19:55 +0300

    Add files via upload

---
+++ HomeWork5.2/HomeWork5.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HomeWork5._2
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод разделяющий слова в предложении
+        /// </summary>
+        /// <param name="text">Строковая переменная, которую нужно разделить в массив</param>
+        /// <returns></returns>
+        static string[] SplitWords(string text)
+        {
+            string[] arrayWord = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var e in arrayWord)
+            {
+                Console.WriteLine(e);
+            }
+            return arrayWord;
+        }
+        /// <summary>
+        /// Метод меняющий слова местами
+        /// </summary>
+        /// <param name="text">Массив строк, который нужно реверсировать</param>
+        /// <returns></returns>
+        static string ReverseWords(string arg)
+        {
+            string[] arrayWord = SplitWords(arg); 
+            Array.Reverse(arrayWord);
+            string newString = string.Join(" ", arrayWord);
+            return newString;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+
+            string arrayWord = ReverseWords(inputPhrase);
+
+            Console.WriteLine(arrayWord);
+
+            Console.ReadKey();
+        }
+    }
+}
